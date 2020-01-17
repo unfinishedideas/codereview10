477cd87fc46ba572a7827fa28d8ea88d0966435b
Peter Wells <petethebeatwells@gmail.com>
2020-01-17 10:43:20 -0800

    adds basic views and CRUD functionality for both products and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,31 +1,55 @@
 class ReviewsController < ApplicationController
 
-  def index
-    # Code for listing all albums goes here.
-  end
+  # def index
+  #   # Code for listing all products goes here.
+  # end
 
   def new
-    # Code for new album form goes here.
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
   end
 
   def create
-    # Code for creating a new album goes here.
-  end
+     @product = Product.find(params[:product_id])
+     @review = @product.reviews.new(review_params)
+     if @review.save
+       redirect_to product_path(@product)
+     else
+       render :new
+     end
+   end
 
-  def edit
-    # Code for edit album form goes here.
-  end
+
+   def edit
+     @product = Product.find(params[:product_id])
+     @review = Review.find(params[:id])
+     render :edit
+   end
 
   def show
-    # Code for showing a single album goes here.
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
   end
 
   def update
-    # Code for updating an album goes here.
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to album_path(@review.product)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # Code for deleting an album goes here.
+    @review = Song.find(params[:id])
+    @review.destroy
+    redirect_to album_path(@review.product)
   end
 
+  private
+  def review_params
+    params.require(:review).permit(:author, :rating, :content_body)
+  end
 end
